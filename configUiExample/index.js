6b9951d35a59879dafd28719176478d2616e7cd0
Andy Perlitch <andyperlitch@gmail.com>
2015-03-11 10:40:06 -0700

    add dimension string property

--- configUiExample/index.js
+++ configUiExample/index.js
@@ -8,8 +8,17 @@ angular.module('andyConfigUiExample', [])
       params: '=',
       type: '@'
     },
-    template: '<label>choose a throughput:</label><select ng-model="throughputParam.value" ng-options="choice as choice for choice in throughputOptions"><option value="">-- choose a throughput --</option></select>',
+    template: '<label>choose a throughput:</label>' +
+              '<select ng-model="throughputParam.value" ng-options="choice for choice in throughputOptions">' +
+              '<option value="">-- choose a throughput --</option>' +
+              '</select><hr>' +
+              '<label>choose dimension strings</label><br>' +
+              '<select ng-repeat-start="dimChoice in dimChoices track by $index" type="text" ng-model="dimChoices[$index]" ng-options="dimOption for dimOption in dimOptions" required><option value="">-- choose a string --</option></select><br ng-repeat-end>' +
+              '<button ng-click="addDimChoice()">add a string</button>'
+              ,
     link: function(scope) {
+
+      // THROUGHPUT STUFF
       scope.throughputOptions = [
         1,
         10,
@@ -19,7 +28,28 @@ angular.module('andyConfigUiExample', [])
       scope.throughputParam = {
         name: 'dt.application.Ingestion.operator.Reader.throughput'
       };
-      scope.params.push(scope.throughputParam);
+
+      // DIMENSIONS
+      // value format: string1|string2|string3|string4
+      scope.dimParam = {
+        name: 'dt.application.Ingestion.operator.Dimensions.format'
+      };
+      scope.dimOptions = [
+        'foo',
+        'bar',
+        'baz',
+        'bat'
+      ];
+      scope.dimChoices = [];
+      scope.addDimChoice = function() {
+        scope.dimChoices.push('');
+      };
+      scope.$watch('dimChoices', function(choices) {
+        scope.dimParam.value = choices.join('|');
+      }, true);
+
+      // Push parameters onto the launchOptions.customParams array
+      scope.params.push(scope.throughputParam, scope.dimParam);
     }
   };
 });
